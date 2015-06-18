9f23cb9736e58321220b2cb296157d7c0680b12b
CarryRee <63755364@qq.com>
2015-06-18 14:03:48 +0800

    Initial Commit

---
+++ CRDrawingBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CRDrawingBoard
+//
+//  Created by CarryRee on 15/6/18.
+//  Copyright (c) 2015年 TONLYELE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CRDrawingBoardTests/CRDrawingBoardTests.m
@@ -0,0 +1,40 @@
+//
+//  CRDrawingBoardTests.m
+//  CRDrawingBoardTests
+//
+//  Created by CarryRee on 15/6/18.
+//  Copyright (c) 2015年 TONLYELE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CRDrawingBoardTests : XCTestCase
+
+@end
+
+@implementation CRDrawingBoardTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
